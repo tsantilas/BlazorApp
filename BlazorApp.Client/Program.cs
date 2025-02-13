4c78f57de74a4b881b71ba07c8c52c60b7e631b6
Michalis Tsantilas <tsantilas.mi@gmail.com>
2025-02-13 20:04:31 +0200

    Add bootstrap on client project

--- BlazorApp.Client/Program.cs
+++ BlazorApp.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
