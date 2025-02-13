16c8a0e3d61d79002468993fc3a89f3975e9a73e
Michalis Tsantilas <tsantilas.mi@gmail.com>
2025-02-13 18:59:24 +0200

    Add mongo db connection settings, mongodb driver and mongodb context

---
+++ BlazorApp/DbContext/MongoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazorApp.DbContext
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("MongoDbSettings:ConnectionString").Value;
+            var databaseName = configuration.GetSection("MongoDbSettings:DatabaseName").Value;
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Customer> Customers => _database.GetCollection<Customer>("Customers");
+    }
+}
